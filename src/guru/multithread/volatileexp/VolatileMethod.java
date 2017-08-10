6722d17e4db12db1a34e86416b37c3caa423d1a5
Srisarguru Sridhar <srisargurusridhar@u.boisestate.edu>
2017-08-10 00:30:56 -0400

    "volatile method info"

--- src/guru/multithread/volatileexp/VolatileMethod.java
+++ src/guru/multithread/volatileexp/VolatileMethod.java
@@ -5,5 +5,11 @@ package guru.multithread.volatileexp;
  *
  */
 public class VolatileMethod {
-  //TODO understand how volatile in methods work in a multi threaded environment. 
+ /*
+  * Now that I have your attention. There is no need for volatile methods
+  * Only fields can be made volatile. 
+  * If methods can be made volatile then it means all threads see a consistent byte code for the method.
+  * But this happens anyway because threads don't see different versions of a class they see the same compiled code 
+  * so no need for volatile methods.
+  */
 }
